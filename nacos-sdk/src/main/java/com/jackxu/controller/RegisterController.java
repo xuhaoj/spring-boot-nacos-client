afaa7cff86a04def46371ea447d5c7a6f4327828
jack xu <779868946@qq.com>
2023-03-30 00:24:09 +0800

    sdk

--- nacos-sdk/src/main/java/com/jackxu/controller/RegisterController.java
+++ nacos-sdk/src/main/java/com/jackxu/controller/RegisterController.java
@@ -6,13 +6,19 @@ import com.alibaba.nacos.api.naming.NamingFactory;
 import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class RegisterController {
 
-    public static void main(String[] args) throws NacosException {
-        NamingService naming = NamingFactory.createNamingService(System.getProperty("serveAddr"));
+    public static void main(String[] args) throws NacosException, IOException {
+        Properties properties = new Properties();
+        properties.setProperty("serverAddr","127.0.0.1:8848");  // 集群地址使用","隔开
+        NamingService naming = NamingFactory.createNamingService(properties);
+
+        //注册1
         naming.registerInstance("nacos.test.3", "11.11.11.11", 8888, "TEST1");
 
         Instance instance = new Instance();
@@ -23,9 +29,16 @@ public class RegisterController {
         Map<String, String> instanceMeta = new HashMap<>();
         instanceMeta.put("site", "et2");
         instance.setMetadata(instanceMeta);
-
-
+        //注册2
         naming.registerInstance("nacos.test.4", instance);
+
+        System.out.println("-----服务发现1-----");
+        System.out.println(naming.getAllInstances("nacos.test.3"));
+        System.out.println("-----服务发现2-----");
+        System.out.println(naming.getAllInstances("nacos.test.4"));
+
+        // 只要这个进程不停止，就意味着nacos client会不断地向nacos server发送心跳  时间间隔
+        System.in.read();
     }
 
 }
